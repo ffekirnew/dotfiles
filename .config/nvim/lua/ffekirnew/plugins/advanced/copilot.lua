0f2bf50939bef799e4e1cc53f0deffe5004bb844
ffekirnew <ffekirnew0808@gmail.com>
2024-05-04 12:57:35 +0300

    fix(nvim): Change acceptance key for copilot

--- .config/nvim/lua/ffekirnew/plugins/advanced/copilot.lua
+++ .config/nvim/lua/ffekirnew/plugins/advanced/copilot.lua
@@ -2,20 +2,43 @@ return {
   "zbirenbaum/copilot.lua",
   lazy = true,
   event = "InsertEnter",
-  opts = {
-    -- Possible configurable fields can be found on:
-    -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
-    suggestion = {
-      enable = false,
-      keymap = {
-        accept = "<C-l>",
+  config = function()
+    require("copilot").setup({
+      -- Possible configurable fields can be found on:
+      -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+      suggestion = {
+        enable = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+        },
       },
-    },
-    panel = {
-      enable = false,
-    },
-    filetypes = {
-      markdown = true,
-    },
-  },
+      panel = {
+        enable = false,
+      },
+      filetypes = {
+        markdown = true,
+      },
+    })
+
+    local keymap = vim.keymap -- for conciseness
+    keymap.set(
+      "n",
+      "<leader>cp",
+      "<cmd>lua require('copilot.suggestion').next()<cr>",
+      { desc = "Show the next copilot suggestion" }
+    )
+    keymap.set(
+      "n",
+      "<leader>cpw",
+      "<cmd>lua require('copilot.suggestion').accept_word()<cr>",
+      { desc = "Accepts the word suggested by copilot" }
+    )
+    keymap.set(
+      "n",
+      "<leader>cpl",
+      "<cmd>lua require('copilot.suggestion').accept_line()<cr>",
+      { desc = "Accepts the line suggested by copilot" }
+    )
+  end,
 }
