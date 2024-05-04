650a103b31e12f169c2c04e956ee210187e46e92
ffekirnew <ffekirnew0808@gmail.com>
2024-05-04 12:57:12 +0300

    feat(nvim): Add vim tmux navigator

---
+++ .config/nvim/lua/ffekirnew/plugins/advanced/vim-tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  config = function()
+    vim.g.tmux_navigator_no_mappings = 1
+    vim.api.nvim_set_keymap("n", "<C-h>", ":TmuxNavigateLeft<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<C-j>", ":TmuxNavigateDown<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<C-k>", ":TmuxNavigateUp<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<C-l>", ":TmuxNavigateRight<CR>", { noremap = true, silent = true })
+  end,
+}
