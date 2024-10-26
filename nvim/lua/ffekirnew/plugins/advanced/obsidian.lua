ee34195b8dd0a5e11971746806b71d4c4e93ccbf
ffekirnew <ffekirnew0808@gmail.com>
2024-10-26 12:32:21 +0300

    Update obsidian config

--- nvim/lua/ffekirnew/plugins/advanced/obsidian.lua
+++ nvim/lua/ffekirnew/plugins/advanced/obsidian.lua
@@ -6,60 +6,75 @@ return {
     -- Required.
     "nvim-lua/plenary.nvim",
   },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "/Users/ffekirnew/Documents/personal",
+  config = function()
+    local obsidian = require("obsidian")
+
+    obsidian.setup({
+      workspaces = {
+        {
+          name = "personal",
+          path = "/Users/ffekirnew/Documents/personal",
+        },
       },
-    },
-    notes_subdir = "inbox",
-    new_notes_location = "notes_subdir",
+      notes_subdir = "inbox",
+      new_notes_location = "notes_subdir",
 
-    disable_frontmatter = true,
-    templates = {
-      subdir = "templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M:%S",
-    },
+      disable_frontmatter = true,
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M:%S",
+      },
 
-    note_id_func = function(title)
-      local suffix = ""
-      local current_datetime = os.date("!%Y-%m-%d-%H%M%S", os.time() + 1 * 3600)
-      if title ~= nil then
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
+      note_id_func = function(title)
+        local suffix = ""
+        local current_datetime = os.date("!%Y-%m-%d-%H%M%S", os.time() + 1 * 3600)
+        if title ~= nil then
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
         end
-      end
-      return current_datetime .. "_" .. suffix
-    end,
+        return current_datetime .. "_" .. suffix
+      end,
 
-    mappings = {
-      -- overrides the 'gf' mapping to work on markdown/wiki links within your vault
-      ["gf"] = {
-        action = function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
+      mappings = {
+        -- overrides the 'gf' mapping to work on markdown/wiki links within your vault
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- toggle check-boxes
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
       },
-      -- toggle check-boxes
-      ["<leader>ch"] = {
-        action = function()
-          return require("obsidian").util.toggle_checkbox()
-        end,
-        opts = { buffer = true },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
       },
-    },
-    completion = {
-      nvim_cmp = true,
-      min_chars = 2,
-    },
-    ui = {
-      -- Disable some things below here because I set these manually for all Markdown files using treesitter
-      checkboxes = {},
-      bullets = {},
-    },
-  },
+      ui = {
+        -- Disable some things below here because I set these manually for all Markdown files using treesitter
+        checkboxes = {},
+        bullets = {},
+      },
+    })
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>on", "<cmd>ObsidianNew<cr>", { desc = "Create a new obsidian note" })
+    keymap.set(
+      "n",
+      "<leader>ont",
+      "<cmd>ObsidianNewFromTemplate<cr>",
+      { desc = "Create a new obsidian note from template" }
+    )
+  end,
 }
